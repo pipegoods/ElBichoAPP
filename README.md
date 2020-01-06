3ebbafa9cf9e56b88fa967b62a39cc5ce3db143f
Pipegoods <pipe.jaider@gmail.com>
2020-01-05 23:18:43 -0500

    Juego 25%

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ElBichoAPP
 Juego de azar sobre EL BICHOOOOO, Ay mi madre el bicho.
+
+Te amo Bicho!!
--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
 var appBicho = new Vue({
     el: '#appBicho',
     data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString()
+      juegoON: true,
+      intentos: 3,
+      puntos: 0,
+      l_arqueria: 0
     },
     
   // define methods under the `methods` object
   methods: {
-    toast: function (event) {
-        M.toast({html: 'I am a toast!'})
+    iniciando: function (event) {
+        M.toast({html: '¡Ay mi madre el Bicho!'});
+        this.juegoON = false;
+    },
+    l_1: function (event) {this.l_arqueria = 1;},
+    l_2: function (event) {this.l_arqueria = 2;},
+    l_3: function (event) {this.l_arqueria = 3;},
+    l_4: function (event) {this.l_arqueria = 4;},
+    chutar: function () {
+      
     }
   }
   })
